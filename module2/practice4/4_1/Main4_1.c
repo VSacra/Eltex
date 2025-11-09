20ea16a143dd01e25a0af4042a52cbadd2dc2796
VSacra <usovavictoria17@gmail.com>
2025-11-09 14:03:09 +0600

    Add files via upload

---
+++ module2/practice4/4_1/Main4_1.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include "PhB4_1.h"
+
+int main() {
+	int v = 0;
+	int i = 1;
+	while (i) {
+		printf("\nЧто хотите сделать?\n1. Добавить контакт.\n2. Редактировать контакт\n3. Удалить контакт\n4. Вывести список контактов\n");
+		scanf("%d", &v);
+		getchar();
+		switch (v) {
+			case 1: {
+				addConsole();
+				break;
+			}
+			case 2: {
+				editConsole();
+				break;
+			}
+			case 3: {
+				deleteConsole();
+				break;
+			}
+			case 4: {
+				View();
+				break;
+			}
+			default: {
+				printf("\nОшибка.\n");
+				abort();
+			}
+		}
+	}
+}
+
+
